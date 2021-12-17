583aa64ad2bc4006acc9d6c244f8eedcce8a2db5
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-17 16:19:43 +0100

    Add: funzione creare box

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//creo una funzione che mi generi i box a cui passerò
+//il div dove metterli e il numero da scriverci dentro
+function newBox(container, n){
+    let box= document.createElement("div");
+    box.className="box";
+    box.innerHTML=n;
+    container.append(box);
+  }
+
 
 //creo gli attributi per richiamare i 3 button del Dom
 const buttonEasy=document.querySelector('.level-1')
@@ -10,13 +19,13 @@ const grid=document.querySelector('.grid');
 
 //mi metto in ascolto del click per selezionare la difficoltà
 buttonEasy.addEventListener("click", function(){
-    
+    //genero 49 box ad ogni click
 })
 
 buttonMedium.addEventListener("click", function(){
-    
+    //genero 81 box ad ogni click
 })
 
 buttonHard.addEventListener("click", function(){
-    
+    //genero 90 box ad ogni click
 })
