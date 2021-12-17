f8f905c2b413fa0289c65dde94c6972084b67f00
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-17 15:58:49 +0100

    Add: pulsanti difficoltà

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+//creo gli attributi per richiamare i 3 button del Dom
+const buttonEasy=document.querySelector('level-1')
+const buttonMedium=document.querySelector('level-2')
+const buttonHard=document.querySelector('level-3')
+
+
+//mi metto in ascolto del click per selezionare la difficoltà
+buttonEasy.addEventListener("click", function(){
+    
+})
+
+buttonMedium.addEventListener("click", function(){
+    
+})
+
+buttonHard.addEventListener("click", function(){
+    
+})
