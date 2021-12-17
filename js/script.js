ece45895c89f8f4c386cb34186483b4863cf1c96
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-17 16:06:41 +0100

    Add: griglia

--- js/script.js
+++ js/script.js
@@ -4,6 +4,9 @@ const buttonEasy=document.querySelector('level-1')
 const buttonMedium=document.querySelector('level-2')
 const buttonHard=document.querySelector('level-3')
 
+//richiamo la grid del Dom da riempire con i miei quadrati
+const grid=document.querySelector('.grid');
+
 
 //mi metto in ascolto del click per selezionare la difficoltà
 buttonEasy.addEventListener("click", function(){
