0b8343418ea5b9fac7afec1c5a2323b7c22dd583
GalluFrancesco <latuamaildigithub@email.comfrancesco.gallu@gmail.com>
2021-12-17 16:07:10 +0100

    Fix: button js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 //creo gli attributi per richiamare i 3 button del Dom
-const buttonEasy=document.querySelector('level-1')
-const buttonMedium=document.querySelector('level-2')
-const buttonHard=document.querySelector('level-3')
+const buttonEasy=document.querySelector('.level-1')
+const buttonMedium=document.querySelector('.level-2')
+const buttonHard=document.querySelector('.level-3')
 
 //richiamo la grid del Dom da riempire con i miei quadrati
 const grid=document.querySelector('.grid');
